b88777a6311adcc4e5a1c0b54e498437c210e8e5
Nolan <nolanwang@cctv.com>
2023-12-11 15:51:01 +0800

    update
    Signed-off-by: Nolan <nolanwang@cctv.com>

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,22 +4,102 @@ pragma solidity ^0.8.13;
 // import ERC2771
 import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
+import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 
 
 
-contract Counter is ERC2771Context {
-    uint256 public number;
 
-    constructor(address trustedForwarder) ERC2771Context(trustedForwarder) {
-        number = 0;
+contract ERC20Tk is ERC2771Context,
+    Multicall
+{
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _totalSupply,
+        address trustedForwarder
+    ) ERC2771Context(trustedForwarder) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
     }
 
-    function increment() public {
-        number++;
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 
-    function setNumber(uint256 x) public {
-        number = x;
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool) {
+        _transfer(from, to, amount);
+        _approve(
+            from,
+            msg.sender,
+            allowance[from][msg.sender] - amount,
+            "ERC20: transfer amount exceeds allowance"
+        );
+        return true;
     }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(msg.sender, spender, amount, "ERC20: approve");
+        return true;
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount,
+        string memory message
+    ) internal {
+        require(spender != address(0), "ERC20: approve to the zero address");
+        allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _msgSender() internal view override returns (address sender) {
+        return super._msgSender();
+    }
+
+    function _msgData() internal view override returns (bytes calldata) {
+        return super._msgData();
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        require(from != address(0), "ERC20: burn from the zero address");
+        balanceOf[from] -= amount;
+        totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
+    }
+
+
+
 }
-
